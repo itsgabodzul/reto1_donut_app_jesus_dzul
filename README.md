af21316e7a47172bae943aca5c70723a2b5c3ef8
Gabriel Dzul <gabriel.dzul.7023@gmail.com>
2024-09-02 14:04:49 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_jesus_dzul
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_jesus_dzul/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_jesus_dzul
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
