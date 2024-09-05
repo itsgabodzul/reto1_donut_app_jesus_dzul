2c207c18f7e841aad95fa6b713e6b50c654f0358
itsgabodzul <gabriel.dzul.7023@gmail.com>
2024-09-05 15:15:35 -0600

    Texto Principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,8 +13,33 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left:24.0),
+          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size:36), onPressed: () { 
+            print('Boton de Menu');
+           },)
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right:24.0),
+          child: IconButton(icon: Icon(Icons.person , color:Colors.grey[800], size:36), onPressed: () { 
+            print('Gracias');
+           },),
+        )],
       ),
+      body: const Column(children: [
+        //Texto "I want to Eat"
+        Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(fontSize: 32),),
+              Text('Eat', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),),
+            ],
+          ),
+        )
+        //Tap Bar
+        //Tap Bar view
+      ],),
     );
   }
 }
