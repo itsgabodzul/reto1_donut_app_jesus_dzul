3536ddefab134473c6de977259f7e0cdc111be68
itsgabodzul <gabriel.dzul.7023@gmail.com>
2024-09-09 15:43:59 -0600

    TabBar_añadido

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:reto1_donut_app_jesus_dzul/tab/burger_tab.dart';
+import 'package:reto1_donut_app_jesus_dzul/tab/donut_tab.dart';
+import 'package:reto1_donut_app_jesus_dzul/tab/pancake_tab.dart';
+import 'package:reto1_donut_app_jesus_dzul/tab/pizza_tab.dart';
+import 'package:reto1_donut_app_jesus_dzul/tab/smoothie_tab.dart';
+import 'package:reto1_donut_app_jesus_dzul/utils/my_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,38 +14,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    //smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+    //pancakes tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.only(left:24.0),
-          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size:36), onPressed: () { 
-            print('Boton de Menu');
-           },)
-        ),
-        actions: [Padding(
-          padding: const EdgeInsets.only(right:24.0),
-          child: IconButton(icon: Icon(Icons.person , color:Colors.grey[800], size:36), onPressed: () { 
-            print('Gracias');
-           },),
-        )],
-      ),
-      body: const Column(children: [
-        //Texto "I want to Eat"
-        Padding(
-          padding: EdgeInsets.all(24.0),
-          child: Row(
-            children: [
-              Text('I want to ', style: TextStyle(fontSize: 32),),
-              Text('Eat', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),),
-            ],
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left:24.0),
+            child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size:36), onPressed: () { 
+              print('Boton de Menu');
+             },)
           ),
-        )
-        //Tap Bar
-        //Tap Bar view
-      ],),
+          actions: [Padding(
+            padding: const EdgeInsets.only(right:24.0),
+            child: IconButton(icon: Icon(Icons.person , color:Colors.grey[800], size:36), onPressed: () { 
+              print('Gracias');
+             },),
+          )],
+        ),
+        body: Column(children: [
+          //Texto "I want to Eat"
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text('I want to ', style: TextStyle(fontSize: 32),),
+                Text('Eat', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),),
+              ],
+            ),
+          ),
+          //Tap Bar
+          TabBar(tabs: myTabs),
+          //Tap Bar view
+          const Expanded(
+            child: TabBarView(children: [
+              //Donut Tap
+              DonutTab(),
+              //Donut Tap
+              BurgerTab(),
+              //Donut Tap
+              SmoothieTab(),
+              //Donut Tap
+              PancakeTab(),
+              //Donut Tap
+              PizzaTab(),
+            ]),
+          ),
+        ],),
+      ),
     );
   }
 }
